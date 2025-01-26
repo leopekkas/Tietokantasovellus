c1e265be7d0e7f82e8231894a65d51250c6eb972
leopekkas <leopekkas@gmail.com>
2025-01-26 21:44:09 +0200

    Add functionality and templates

--- routes.py
+++ routes.py
@@ -45,29 +45,28 @@ def register():
             return render_template("error.html", message="Error showing the registration page.")
 
 
-@app.route("/new")
-def new():
-    result = db.session.execute(text("SELECT name FROM areas"))
-    areas = result.fetchall()
-    return render_template("new.html", areas=areas)
+@app.route("/new/<int:id>")
+def new(id):
+    result = db.session.execute(text("SELECT name FROM areas WHERE id=:id"))
+    area = result.fetchone()[0]
+    return render_template("new.html", if=if, area=area)
 
 @app.route("/create", methods=["POST"])
 def create():
     topic = request.form["topic"]
+    if (topic == ""):
+        return render_template("error.html", message="Please mark a topic for your new conversation")
     message = request.form["message"]
-    name = request.form["area"]
-    sql = "SELECT id FROM areas WHERE name=:name"
-    result = db.session.execute(sql, {"name":name})
-    area_id = result.fetchone()[0]
+    area_id = request.form["id"]
     starter_id = users.user_id()
     sql = "INSERT INTO topics(topic, starter_id, area_id, created_at) VALUES (:topic, :starter_id, :area_id, NOW()) RETURNING id"
-    result = db.session.execute(sql, {"topic":topic, "area":name, "starter_id":starter_id, "area_id":area_id})
+    result = db.session.execute(sql, {"topic":topic, "starter_id":starter_id, "area_id":area_id})
     topic_id = result.fetchone()[0]
+    db.session.commit()
     if message != "":
         sql = "INSERT INTO messages(message, sender_id, topic_id, visibility, sent_at) VALUES (:message, :sender_id, :topic_id, :visibility, NOW())"
         db.session.execute(sql, {"message":message, "sender_id":starter_id, "topic_id":topic_id, "visibility":1})
-
-    db.session.commit()
+    	db.session.commit()
     return redirect("/")
 
 @app.route("/convo/<int:id>")
@@ -75,6 +74,8 @@ def convo(id):
     sql ="SELECT id, topic FROM topics WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     topic = result.fetchone()
+    if topic == None:
+	return render_template("error.html", message="No conversations in this topic")
     visibility=1
     sql = "SELECT messages.id, messages.message, users.username, users.id FROM messages, users WHERE messages.topic_id=:id AND visibility=:visibility AND messages.sender_id=users.id ORDER BY sent_at ASC"
     result = db.session.execute(sql, {"id":id, "visibility":visibility})
@@ -105,3 +106,77 @@ def delete():
     result = db.session.execute(sql, {"id":message_id})
     db.session.commit()
     return redirect("/convo/"+str(id))
+
+@app.route("/search")
+def search():
+    word = request.args["word"]
+    visibility=1
+    if word == "":
+        return redirect("/")
+    sql = "SELECT messages.message, users.username, topics.id FROM messages, users, topics WHERE visibility=:visibility AND messages.sender_id=users.id AND topics.id=messages.topic_id AND messages.message LIKE :word ORDER BY sent_at ASC"
+    result = db.session.execute(sql, {"visibility":visibility, "word":"%"+word+"%"})
+    messages = result.fetchall()
+    if len(messages)==0:
+        return render_template("error.html", message = "No messages found with this search")
+    return render_template("search.html", messages=messages)
+
+@app.route("/edit/<int:id>", methods=["POST"])
+def update(id):
+    sql = "SELECT message FROM messages WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    message = result.fetchone()[0]
+    return render_template("update.html", id=id, message=message)>
+
+@app.route("/update_message", methods=["POST"])
+def update_message():
+    message = request.form["message"]
+    id = request.form["id"]
+    sql = "UPDATE messages SET message=:message WHERE id=:id"
+    db.session.execute(sql, {"message":message, "id":id})
+    db.session.commit()
+    sql = "SELECT topic_id FROM messages WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic_id = result.fetchone()[0]
+    return redirect("/convo/"+str(topic_id))
+
+@app.route("/create_area")
+def create_area():
+    return render_template("create_area.html")
+
+@app.route("/create_private_area", methods=["POST"])
+def create_private():
+    name = request.form["theme"]
+    user_id = users.user_id()
+    sql = "INSERT INTO areas(name, user_id) VALUES (:name, :user_id) RETURNING id"
+    result = db.session.execute(sql, {"name":name, "user_id":user_id})
+    area_id = result.fetchone()[0]
+    db.session.commit()
+    sql = "INSERT INTO accessrights(area_id, user_id) VALUES (:area_id, :user_id)"
+    db.session.execute(sql, {"area_id":area_id, "user_id":user_id}) 
+    db.session.commit()
+    return redirect("add_users/"+str(area_id))
+
+@app.route("/add_users/<int:id>")
+def add_users(id):
+    return render_template("add_users.html", id=id)
+
+@app.route("/add", methods=["POST"])
+def add():
+    username = request.form["username"]
+    if username == "" or username == None:
+        return render_template("error.html", message = "You have to write a username")
+    sql = "SELECT username FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    name = result.fetchone()
+    if name == None or name == "":
+        return render_template("error.html", message = "There is no user with that username")
+    area_id = request.form["id"]
+    sql = "SELECT id FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user_id = result.fetchone()[0]
+    if users.check_rights(user_id, area_id):
+        return render_template("add_users.html", id=area_id, message="That user already has accessrights")
+    sql = "INSERT INTO accessrights(area_id, user_id) VALUES (:area_id, :user_id)"
+    db.session.execute(sql, {"area_id":area_id ,"user_id":user_id})
+    db.session.commit()
+    return render_template("add_users.html", id=area_id)
