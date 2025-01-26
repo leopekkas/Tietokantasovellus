63d46825c5785773691f2f6345c8b8e1f9a1f1f4
leopekkas <leopekkas@gmail.com>
2025-01-26 13:47:06 +0200

    Update fly.io setup

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ CREATE TABLE areas(id SERIAL PRIMARY KEY, name TEXT, user_id INTEGER REFERENCES 
 CREATE TABLE topics(id SERIAL PRIMARY KEY, topic TEXT, starter_id INTEGER REFERENCES users, area_id INTEGER REFERENCES areas, created_at TIMESTAMP);
 CREATE TABLE accessRights(area_id INTEGER REFERENCES areas, user_id INTEGER REFERENCES users);
 CREATE TABLE messages(id SERIAL PRIMARY KEY, message TEXT NOT NULL, sender_id INTEGER REFERENCES users, topic_id INTEGER REFERENCES topics, visibility INTEGER, sent_at TIMESTAMP);
+INSERT INTO areas (name) VALUES ('Hobbies');
